99fe8624190baa63db97e6d02c0e41d902072573
palvevaishnav <palvevaishnav09@gmail.com>
2025-05-14 15:38:29 +0530

    V-0.1.2

--- app/page.tsx
+++ app/page.tsx
@@ -7,30 +7,38 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-[#f8f5f0]">
       {/* Header */}
-      <header className="border-b border-[#e0d6c8] bg-[var(--brandOne)] text-white">
-        <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+      <header className="border-b border-[#e0d6c8] bg-[var(--brandOne)] text-white fixed z-10 w-full top-0 ">
+        <div className="container mx-auto px-4 py-2 flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <Music className="h-6 w-6 text-[#8b0000]" />
-            <span className="font-serif text-xl font-medium">SparshNinaad</span>
+            <div className="h-20 w-40">
+              <Image
+                src="/logo6.png"
+                alt="Education initiative"
+                width={60}
+                height={20}
+                className="rounded-lg w-full h-full"
+              />
+            </div>
+            {/* <span className="font-serif text-xl font-medium">SparshNinaad</span> */}
           </div>
           <nav className="hidden md:flex space-x-8">
-            <Link href="#about" className="text-sm font-medium hover:text-[#8b0000] transition-colors">
+            <Link href="#about" className="text-lg font-medium hover:bg-[var(--brandTwo)] hover:text-black p-2 rounded-lg transition-colors">
               About
             </Link>
-            <Link href="#upcoming" className="text-sm font-medium hover:text-[#8b0000] transition-colors">
+            <Link href="#upcoming" className="text-lg font-medium hover:bg-[var(--brandTwo)] hover:text-black p-2 rounded-lg transition-colors">
               Jugalbandi 2025
             </Link>
-            <Link href="#vision" className="text-sm font-medium hover:text-[#8b0000] transition-colors">
+            <Link href="#vision" className="text-lg font-medium hover:bg-[var(--brandTwo)] hover:text-black p-2 rounded-lg transition-colors">
               Our Vision
             </Link>
-            <Link href="#gallery" className="text-sm font-medium hover:text-[#8b0000] transition-colors">
+            <Link href="#gallery" className="text-lg font-medium hover:bg-[var(--brandTwo)] hover:text-black p-2 rounded-lg transition-colors">
               Gallery
             </Link>
-            <Link href="#contact" className="text-sm font-medium hover:text-[#8b0000] transition-colors">
+            <Link href="#contact" className="text-lg font-medium hover:bg-[var(--brandTwo)] hover:text-black p-2 rounded-lg transition-colors">
               Contact
             </Link>
           </nav>
-          <Button className=" hover:bg-[#8b0000] hover:text-white bg-[var(--brandTwo)]">
+          <Button className=" hover:bg-[#8b0000] hover:text-white bg-[var(--brandTwo)] text-lg">
             Jugalbandi 2025
           </Button>
         </div>
@@ -46,7 +54,8 @@ export default function Home() {
             className="object-cover"
           /> */}
         </div>
-        <div className="container mx-auto px-4 relative z-10">
+        {/* add z-10 if need to add a background image */}
+        <div className="container mx-auto px-4 relative">
           <div className="max-w-3xl mx-auto text-center">
             <h1 className="font-serif text-4xl md:text-6xl font-bold text-[#1a1a1a] mb-6">Soul of Music. <br /> Echoes of Culture. </h1>
             <p className="text-lg md:text-xl text-[#4a4a4a] mb-8 leading-relaxed">
@@ -258,7 +267,7 @@ export default function Home() {
           <div className="grid md:grid-cols-3 gap-8">
             <div className="bg-[#ffffff] p-6 rounded-lg shadow-sm border border-[#e0d6c8]">
               <Image
-                src="/g1.jpg"
+                src="/g2.jpg"
                 alt="Education initiative"
                 width={400}
                 height={300}
@@ -271,7 +280,7 @@ export default function Home() {
             </div>
             <div className="bg-white p-6 rounded-lg shadow-sm border border-[#e0d6c8]">
               <Image
-                src="/g2.jpg"
+                src="/g1.jpg"
                 alt="Healthcare support"
                 width={400}
                 height={300}
